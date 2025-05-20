5322ed752f601db0dc401439d54aba6a29375c99
Danar09022006 <rohmanrozaqi8@gmail.com>
2025-05-20 10:18:43 +0700

    membuat void  peak

--- stack.cpp
+++ stack.cpp
@@ -39,12 +39,40 @@ public:
     return value;
 } 
     
-    
-    
-    
-    
-    
+    void pop()
+    {
+        if (isEmpty())
+        {
+            cout << "Stack is Empty" << endl;
+        }
 
+        cout << "Popped Value: " << top->data << endl;
+        top = top->next;
+
+    }
+
+    void peak()
+    {
+        if (top == NULL)
+        {
+            cout << "List is Empty." << endl;
+        }
+        else
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }
+   
+   
+   
+    }   
+        
+    
     
     
 }
