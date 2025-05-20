ea5813df4d9f9fccab7845d84430f76a2eab10fd
Danar09022006 <rohmanrozaqi8@gmail.com>
2025-05-20 10:08:27 +0700

    membuat void pop

--- stack.cpp
+++ stack.cpp
@@ -29,9 +29,15 @@ public:
         top = NULL;
     }
 
-    
-    
-    
+   int push(int value)
+{
+    Node *newNode = new Node();
+    newNode->data = value;
+    newNode->next = top;
+    top = newNode;
+    cout << "Push Value: " << value << endl;
+    return value;
+} 
     
     
     
@@ -39,12 +45,6 @@ public:
     
     
 
-    void pop()
-    {
-        if (isEmpty())
-        {
-            cout << "Stack is Empty" << endl;
-
-        }
-    }
+    
+    
 }
