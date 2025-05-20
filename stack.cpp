238375c37ecef09f5d7e4d2d589ff293728081a5
Danar09022006 <rohmanrozaqi8@gmail.com>
2025-05-20 09:59:07 +0700

    class private

--- stack.cpp
+++ stack.cpp
@@ -3,13 +3,33 @@
 #include <iostream>
 using namespace std;
 
+class Node
+{
+public:
+    int data;
+    Node *next;
 
+    Node()
+    {
 
-
-
-
-
-
-
-
+        next = NULL;
+    }
 };
+
+
+
+
+
+
+public:
+    Stack()
+    {
+
+        top = NULL;
+    }
+
+    int push(int value)
+    {
+
+    }
+}
